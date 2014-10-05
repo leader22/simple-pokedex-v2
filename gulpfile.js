3f12aa69516c005cefb9aa2711710dd4165adc95
@leader22 <y.sugiura.0316@gmail.com>
2014-10-05 18:39:47 +0900

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,68 @@
+// Gulpfile
+// -----------------------------------------------------------------
 'use strict';
 
+
+// Deps
+// -----------------------------------------------------------------
 var gulp    = require('gulp');
 var gutil   = require('gulp-util');
+var webpack = require('webpack');
 var compass = require('gulp-compass');
-var webpack = require('webpack'),
-    webpackConf = require('./webpack.config.js');
 
-var webpackCompiler = webpack(webpackConf);
 
+// Conf
+// -----------------------------------------------------------------
+var webpackConf = {
+    entry: {
+        main: './client/main',
+    },
+    output: {
+        path:     __dirname + "/static/js",
+        filename: '[name].js'
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            warnings: true,
+            compress: {
+                drop_console: true
+            },
+            report: 'min'
+        })
+    ]
+};
+
+var compassConf = {
+    style:    'compressed',
+    comments: false,
+    image:    'asset',
+    css:      'static/css',
+    sass:     'asset/scss'
+};
+
+
+// Tasks
+// -----------------------------------------------------------------
 gulp.task('webpack', function(callback) {
-    webpackCompiler.run(function(err, stats) {
+    webpack(webpackConf, (function(err, stats) {
         if(err) throw new gutil.PluginError('webpack', err);
         gutil.log("[webpack]", stats.toString({
             colors: true
         }));
         callback();
-    });
+    }));
 });
 
 gulp.task('compass', function() {
     gulp.src('asset/scss/*.scss')
-        .pipe(compass({
-            style:    'compressed',
-            comments: false,
-            css:      'static/css',
-            sass:     'asset/scss'
-        }))
+        .pipe(compass(compassConf))
         .pipe(gulp.dest('static/css'));
 });
 
+
+// Export
+// -----------------------------------------------------------------
 gulp.task('default', ['compass', 'webpack']);
 
 gulp.task('dev', ['compass', 'webpack'], function() {
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    entry: {
-        main: './client/main',
-    },
-    output: {
-        path:     __dirname + "/static/js",
-        filename: '[name].js'
-    }
-};
